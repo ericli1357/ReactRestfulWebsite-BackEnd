b3fbad3c76870d53c7e179892ec9c35f683db792
Eric Li <ericli1357@gmail.com>
2022-05-15 16:12:43 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EmailService;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactRestfulWebsite.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IEmailSender _emailSender;
+
+        public HomeController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [EnableCors("AllowOrigin")]
+        [HttpPost("sendEmail")]
+        public  ActionResult SendEmail()
+        {
+            var message = new Message(new string[] { "erichxli@gmail.com" }, "Test email", "This is the content from our email.");
+            _emailSender.SendEmail(message);
+            return NoContent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+using EmailService;
+using Microsoft.Extensions.Configuration;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://example.com",
+                                              "http://www.contoso.com");
+                      });
+});
+
+builder.Services.AddSingleton(builder.Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>());
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHttpsRedirection();
+}
+app.UseRouting();
+app.UseCors(MyAllowSpecificOrigins);
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=SendEmail}"
+    );
+
+app.Run();
